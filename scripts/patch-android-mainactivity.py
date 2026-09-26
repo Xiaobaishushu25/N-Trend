172478c30ae79e7f1583afb4d6e429696165c238
Pang <2055761346@qq.com>
2026-09-26 23:13:20 +0800

    build apk

--- scripts/patch-android-mainactivity.py
+++ scripts/patch-android-mainactivity.py
@@ -13,7 +13,6 @@ import android.os.Bundle
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
-import app.tauri.TauriActivity
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
